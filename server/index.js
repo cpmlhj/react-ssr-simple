eb8ff6fe31b1b3013dce327f95f97ab2556a5f5c
cpmlhj <345829331@qq.com>
2019-12-15 18:07:06 +0800

    (#)微调

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import routes from '../App'
 import express from 'express'
 import { StaticRouter, matchPath, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import proxy  from 'http-proxy-middleware'
+import proxy from 'http-proxy-middleware'
 import { getServerStore } from '../store/store'
 import Headers from '../component/header'
 const app = express()
@@ -18,9 +18,10 @@ const proxyTable = {
 Object.keys(proxyTable).forEach(k => app.use(proxy(k, proxyTable[k])))
 app.use(express.static('public'))
 app.get('*', (req, res) => {
+       if(req.path === '/favicon.ico')  return
       // 把react组件解析成HTML
       const promise = []
-
+      let content = ''
       routes.some(route => {
             const match = matchPath(req.path, route)
             if (match) {
@@ -29,38 +30,43 @@ app.get('*', (req, res) => {
             }
       })
       // console.log(Promise.allSettled([]))
-      Promise.all(promise).then(e => {
-            //
-      }).catch((e) => {
-           throw Error(e)
-      }).finally(() => {
-            const content = renderToString(
+      Promise.all(promise.map(p => p.catch((e) => { return e }))).then(e => {
+            if (e.length === 1) {
+                  //
+                  return
+            }
+            content = renderToString(
                   <Provider store={store}>
 
                         <StaticRouter localtion={req.url}>
                               <Headers />
                               {
-                                    routes.map(r => <Route {...r}></Route>)
+                                    routes.map((r, index) => {
+                                          return <Route {...r}></Route>
+                                    })
                               }
                         </StaticRouter>
 
                   </Provider>
             )
+      }).finally(() => {
+            console.log(content)
             res.send(`
-                <html>
-                   <head>
-                      <meta charset='utf-8' />
-                      <title>reacr ssr</title>
-                   </head>
-                   <body>
-                       <div id='root'>${content}</div>
-                       <script>
-                       window._context = ${JSON.stringify(store.getState())}
-                       </script>
-                       <script src="bundle.js"></script>
-                   </body>
-                </html>
-            `)
+            <html>
+               <head>
+                  <meta charset='utf-8' />
+                  <title>reacr ssr</title>
+               </head>
+               <body>
+                   <div id='root'>${content}</div>
+                   <script>
+                   window._context = ${JSON.stringify(store.getState())}
+                   </script>
+                   <script src="bundle.js"></script>
+               </body>
+            </html>
+        `)
       })
+
 })
 app.listen(9033)
